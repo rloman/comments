e7b973d37426bd0286d2967adcd4fa9a06dd1e66
Raymond Loman <raymond@carpago.nl>
2018-02-22 09:33:05 +0100

    Add working mysql / jdbc demo. Done

--- src/main/java/nl/carpago/Application.java
+++ src/main/java/nl/carpago/Application.java
@@ -1,8 +1,13 @@
 package nl.carpago;
 
+import java.sql.SQLException;
+
+import nl.carpago.util.MySQLAccess;
+
 public class Application {
 
-   public static void main(String[] args) {
-      System.out.println("Hello World!");
+   public static void main(String[] args) throws ClassNotFoundException, SQLException {
+      MySQLAccess dao = new MySQLAccess();
+      dao.readDataBase();
    }
 }
